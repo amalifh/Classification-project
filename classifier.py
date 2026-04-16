e64b8dddd25255736423d3f5b2d7dad8f6c55162
amalifh <amalifh@stud.ntnu.no>
2026-04-16 20:13:44 +0200

    working on classifier

--- classifier.py
+++ classifier.py
@@ -1,21 +1,57 @@
 import numpy as np
+import pandas as pd
 def score_function(weight, bias, data):
-    print(data)
     w_t = np.transpose(weight)
     w_tx = np.matmul(w_t, data)
     return w_tx + bias
    
+def check_label_data(file):
+    df = pd.read_csv(file)
+    df['iris_type'] = df['iris_type'].str.lower().map({
+    'setosa': 0,
+    'versicolor': 1,
+    'virginica': 2
+    })
+    return df
 
-def trainer(file):
+def MSE(prediction, target):
+    return (prediction-target)*(prediction-target)
+
+def update_weights(prediction, target):
     
-    with open(file, 'r') as f:
-        data = f.readlines()
-        for line in data:
-            clean_data = line.strip()
-            use_data = clean_data.split(',')
-            w = np.zeros(len(use_data))
-            print(len(use_data))
-            score1 = score_function(w, 0, clean_data)
-            score2 = score_function(w, 0, clean_data)
-            score3 = score_function(w, 0, clean_data)
-    return score1, score2, score3
+
+#three separate binary classifiers
+def iris_classifier(file):
+    feature = np.zeros(4)
+    df = check_label_data(file)
+    print(df)
+    w1 = np.zeros(4)
+    w2 = np.zeros(4)
+    w3 = np.zeros(4)
+
+    b1 = 0
+    b2 = 0
+    b3 = 0
+
+    alpha = 0.01
+
+    for _, row in df.iterrows():
+        feature = row[:4]
+        target = row.iloc[4]
+        score1 = score_function(w1, b1, feature)
+        score2 = score_function(w2, b2, feature)
+        score3 = score_function(w3, b3, feature)
+        scores = [score1, score2, score3]
+        predicted = np.argmax(scores)
+        
+        #if wrong, increase weight of true and decrease weight of predicted
+        if predicted != target:
+            error = predicted - target
+            MSE = error * error
+            gradient_w = error * feature
+
+            if predicted == 0:
+                w1 = w1
+    print(f'predicted: {predicted} and target {target}')
+
+iris_classifier('class_2_training')
--- read_data.py
+++ read_data.py
@@ -1,83 +1,15 @@
-import numpy as np
-import csv
-import json
-import os
+import pandas as pd
 
 #klassene inneholder infoen: sepal length, sepal width, petal length, petal width
-def create_set(size, file, name_set):
+def create_set(size, file, name_new, type_class):
+    cols = ['sepal_l', 'sepal_w', 'petal_l', 'petal_w', 'iris_type']
+    df = pd.read_csv(file,nrows=size, names=cols)
+    df['iris_type'] = type_class
+    df.to_csv(name_new, columns=cols, index=False)
 
-    with open(file, 'r')as f:
-        raw_data = np.array(f.readlines())
-        new_set = []
-
-        for i, line in enumerate(raw_data[:size]):
-            data = line.strip()
-            new_set.append(data)
-    with open(name_set, 'w+') as f:
-        for element in new_set:
-            f.write('%s\n' %element)
-
-
-3
-def create_set(file, size, name, type):
-    
-    with open(file, 'r') as f:
-        new_set = {}
-        data_set = []
-        raw_data = np.array(f.readlines())
-        for line in raw_data[:size]:
-            data = line.strip()
-            clean_data= []
-            for element in data:
-                clean_data.append(float(element))
-
-            data_set.append(clean_data)
-
-    new_set = { 
-        "Class" : type,
-        "data" : data_set
-    }
-
-    with open(name, 'w') as f:
-        json.dump(new_set, f)
-
-def create_set4(file, size, name, type_):
-    data_set = []
-
-    with open(file, 'r') as f:
-        for i, line in enumerate(f):
-            if i >= size:
-                break
-            clean_data = [float(x) for x in line.strip().split(',')]
-            data_set.append(clean_data)
-
-    new_set = {
-        "Class": type_,
-        "data": data_set
-    }
-
-    with open(name, 'w') as f:
-        json.dump(new_set, f, indent=4)
-
-def create_set2(file, size, name, class_label):
-    with open(file, 'r') as f_in, open(name, 'w', newline='') as f_out:
-        writer = csv.writer(f_out)
-
-        for i, line in enumerate(f_in):
-            if i >= size:
-                break
-
-            # split numbers (change ',' if needed)
-            row = [float(x) for x in line.strip().split(',')]
-            
-            # append class label
-            row.append(class_label)
-
-            writer.writerow(row)
-
-create_set2("class_1", 30, "class1_training", "Setosa")
-create_set2('class_1', 20, 'class1_testing', 'Setosa')
-create_set2('class_2', 30, 'class2_training', 'Versicolor'),
-create_set2('class_2', 20,  'class2_testing', 'Versicolor')
-create_set2('class_3', 30, 'class3_training', 'Virginica' )
-create_set2('class_3', 20, 'class3_testing', 'Virginica')
+create_set(30, 'class_1', 'class_1_training', 'Setosa')
+create_set(20,'class_1', 'class_1_testing', 'Setosa')
+create_set(30, 'class_2', 'class_2_testing', 'Versicolor')
+create_set(20, 'class_2', 'class_2_training', 'Versicolor')
+create_set(30, 'class_3', 'class_3_training', 'Virginica')
+create_set(20, 'class_3', 'class_3_testing', 'Versicolor')
