fixing gradient

classifier.py
@@ -17,18 +17,21 @@ def check_label_data(file):
 def MSE(prediction, target):
     return (prediction-target)*(prediction-target)
 
-def update_weights(prediction, target):
-    
+"""def update_weights(prediction, target):
+    error = prediction - target
+    if prediction == 2:
+        if target == 1:
+            w1 """
+
 
 #three separate binary classifiers
 def iris_classifier(file):
     feature = np.zeros(4)
     df = check_label_data(file)
-    print(df)
     w1 = np.zeros(4)
     w2 = np.zeros(4)
     w3 = np.zeros(4)
-
+    
     b1 = 0
     b2 = 0
     b3 = 0
@@ -36,8 +39,8 @@ def iris_classifier(file):
     alpha = 0.01
 
     for _, row in df.iterrows():
-        feature = row[:4]
-        target = row.iloc[4]
+        feature = row.values[:4]
+        target = int(row.iloc[4])
         score1 = score_function(w1, b1, feature)
         score2 = score_function(w2, b2, feature)
         score3 = score_function(w3, b3, feature)
@@ -48,10 +51,28 @@ def iris_classifier(file):
         if predicted != target:
             error = predicted - target
             MSE = error * error
-            gradient_w = error * feature
-
+            gradient_w = MSE * feature
             if predicted == 0:
-                w1 = w1
-    print(f'predicted: {predicted} and target {target}')
+                w1 = w1 - alpha*gradient_w
+                if target == 1:
+                    w2 = w2 + alpha*gradient_w
+                if target == 2:
+                    w3 = w3 + alpha*gradient_w
 
+            if predicted == 1:
+                w2 = w2 - alpha*gradient_w
+                if target == 0:
+                    w1 = w1 + alpha*gradient_w
+                if target == 2:
+                    w3 = w3 + alpha*gradient_w
+
+            if predicted == 2:
+                w3 = w3 - alpha*gradient_w
+                if target == 0:
+                    w1 = w1 + alpha*gradient_w
+                if target == 1:
+                    w2 = w2 + alpha*gradient_w
+   # print(f'some information {w1}')
+    print(f'predicted: {predicted} and target {target}')
+    print(f'w1: {w1}, w2: {w2} and w3: {w3}')
 iris_classifier('class_2_training')